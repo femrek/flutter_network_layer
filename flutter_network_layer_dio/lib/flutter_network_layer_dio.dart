66c5b0c9742a88663c44f18561fe5aa21e81888d
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2025-01-04 16:03:07 +0300

    feat: dio impl exports core package.

--- flutter_network_layer_dio/lib/flutter_network_layer_dio.dart
+++ flutter_network_layer_dio/lib/flutter_network_layer_dio.dart
@@ -1,3 +1,5 @@
+export 'package:flutter_network_layer_core/flutter_network_layer_core.dart';
+
 export 'src/dio_network_invoker.dart';
 export 'src/logging/log_level.dart';
 export 'src/logging/on_dio_log.dart';
