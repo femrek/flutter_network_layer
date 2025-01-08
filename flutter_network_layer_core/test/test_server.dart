c0ed14cf816a1e1c805db73f33707157292db8a3
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2025-01-08 04:13:50 +0300

    style: unncessary commend block.

--- flutter_network_layer_core/test/test_server.dart
+++ flutter_network_layer_core/test/test_server.dart
@@ -6,10 +6,7 @@ abstract final class TestServer {
   static Future<HttpServer> bindServer({
     required OnRequestCallback onRequest,
   }) async {
-    final server = await HttpServer.bind(
-      InternetAddress.loopbackIPv4,
-      0,
-    );
+    final server = await HttpServer.bind(InternetAddress.loopbackIPv4, 0);
 
     server.listen((HttpRequest request) {
       onRequest(request);
@@ -17,36 +14,4 @@ abstract final class TestServer {
     });
     return server;
   }
-// }) async {
-//     final server = await HttpServer.bind(
-//       InternetAddress.loopbackIPv4,
-//       8080,
-//     );
-//
-//     server.listen((HttpRequest request) {
-//       if (request.method == 'GET') {
-//         switch (request.uri.path) {
-//           case '/basic_test':
-//             request.response
-//               ..statusCode = HttpStatus.ok
-//               ..write('{"field1": "value1"}');
-//           case '/basic_test/1':
-//             request.response
-//               ..statusCode = HttpStatus.ok
-//               ..write('{"field1": "value1"}');
-//           default:
-//             request.response
-//               ..statusCode = HttpStatus.notFound
-//               ..write('Not Found');
-//         }
-//       } else {
-//         request.response
-//           ..statusCode = HttpStatus.methodNotAllowed
-//           ..write('Unsupported request: ${request.method}.');
-//       }
-//
-//       request.response.close();
-//     });
-//     return server;
-//   }
 }
