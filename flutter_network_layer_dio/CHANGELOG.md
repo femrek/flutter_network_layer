01a71f36b1c8bd0ae5f71e6cd6ceaaf368bc6450
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2025-01-12 18:38:59 +0300

    feat: reads CHANGELOG.md file when creating github releases.

--- flutter_network_layer_dio/CHANGELOG.md
+++ flutter_network_layer_dio/CHANGELOG.md
@@ -1,7 +1,6 @@
 ## 0.2.0
 
 * flutter_network_layer_core dependency updated to v0.2.0
-* 
 
 ## 0.1.3
 
---
+++ scripts/change_log_reader.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+# get first argument as the file name
+file = sys.argv[1]
+
+# check if the file exists
+if not os.path.isfile(file):
+    sys.stderr.write("File not found!\n")
+    sys.exit(1)
+
+# get second argument as the version
+version = sys.argv[2]
+
+# check if the version is empty
+if not version:
+    sys.stderr.write("Version input not found!\n")
+    sys.exit(1)
+
+# get the change log
+with open(file, 'r') as f:
+    change_log = f.read()
+
+# get the change log for the version
+start = -1
+end = -1
+file_lines = change_log.split('\n')
+for (i, line) in enumerate(file_lines):
+    if start < 0:
+        if version in line:
+            start = i
+    elif line.startswith('## '):
+        end = i
+        break
+
+if start < 0:
+    sys.stderr.write("Version not found in file!\n")
+    sys.exit(1)
+
+if end < 0:
+    end = len(file_lines)
+
+version_change_log = '\n'.join(file_lines[start + 1:end])
+
+# remove the empty lines
+version_change_log = version_change_log.strip()
+
+# check if the version change log is empty
+if not version_change_log:
+    sys.stderr.write("Version not found in lines!\n")
+    sys.exit(1)
+
+# print the version change log
+print(version_change_log)
