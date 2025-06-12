4314ffa57a978d241785e699827909d707bfaefc
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2025-06-12 15:06:13 +0300

    test: root test extended.

--- test/flutter_network_layer_test.dart
+++ test/flutter_network_layer_test.dart
@@ -4,13 +4,16 @@ import 'package:http_test_server/http_test_server.dart';
 import 'package:test/test.dart';
 
 import 'data/request/request_test_1.dart';
+import 'data/response/response_test_1.dart';
+
+List<INetworkInvoker> _createInvokers() => <INetworkInvoker>[
+      DioNetworkInvoker(),
+      DartNetworkInvoker(),
+    ];
 
 void main() {
   group('Basic Test', () {
-    <INetworkInvoker>[
-      DioNetworkInvoker(),
-      DartNetworkInvoker(),
-    ].forEach((e) {
+    _createInvokers().forEach((e) {
       test('Invoker: ${e.runtimeType}', () async {
         // setup server
         final server = await TestServer.createHttpServer(events: [
@@ -38,6 +41,99 @@ void main() {
           },
         );
         expect(response.field1, 'value1');
+
+        await server.close(force: true);
+      });
+    });
+  });
+
+  group('Unresolved Host Test', () {
+    _createInvokers().forEach((e) {
+      test('Invoker: ${e.runtimeType}', () async {
+        // init invoker with unresolved host
+        await e.init('http://unresolved-host');
+
+        // send request
+        final responseResult = await e.request(RequestTest1());
+
+        responseResult.when(
+          success: (_) {
+            fail('The request should fail');
+          },
+          error: (error) {
+            expect(
+              error.isFromLocal,
+              isTrue,
+              reason: 'The error should be from local',
+            );
+          },
+        );
+      });
+    });
+  });
+
+  group('Invalid Response Type Test', () {
+    _createInvokers().forEach((e) {
+      test('Invoker: ${e.runtimeType}', () async {
+        // setup server
+        final server = await TestServer.createHttpServer(events: [
+          StandardServerEvent(
+            matcher: ServerEvent.standardMatcher(paths: ['/test1']),
+            handler: (request) {
+              return 'Invalid JSON';
+            },
+          ),
+        ]);
+
+        // init invoker
+        await e.init('http://localhost:${server.port}');
+
+        // send request
+        final responseResult = await e.request(RequestTest1());
+
+        responseResult.when(
+          success: (_) {
+            fail('The request should fail');
+          },
+          error: (error) {
+            expect(error, isA<ErrorResponseResult<ResponseTest1>>());
+          },
+        );
+
+        await server.close(force: true);
+      });
+    });
+  });
+
+  group('Network Error Response Test', () {
+    _createInvokers().forEach((e) {
+      test('Invoker: ${e.runtimeType}', () async {
+        // setup server
+        final server = await TestServer.createHttpServer(events: [
+          StandardServerEvent(
+            matcher: ServerEvent.standardMatcher(paths: ['/test1']),
+            handler: (request) {
+              return 'Not Found';
+            },
+            responseStatusCode: 404,
+          ),
+        ]);
+
+        // init invoker
+        await e.init('http://localhost:${server.port}');
+
+        // send request
+        final responseResult = await e.request(RequestTest1());
+
+        responseResult.when(
+          success: (_) {
+            fail('The request should fail');
+          },
+          error: (error) {
+            expect(error, isA<ErrorResponseResult<ResponseTest1>>());
+            expect(error.statusCode, 404);
+          },
+        );
 
         await server.close(force: true);
       });
